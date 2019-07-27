d5307a8c630a5c5184d52d9b79f3f88ad205a96b
Chari16 <bhaveshchari1997@gmail.com>
2019-07-27 21:03:45 +0530

    Uploading images and Deleting images

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,3 +1,4 @@
+const multer = require('multer')
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -77,6 +78,33 @@ router.delete('/users/me',auth, async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+})
+
+
+const upload = multer({
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined,true)
+    }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+},(error,req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 module.exports = router
